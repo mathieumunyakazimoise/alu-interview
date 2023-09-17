03066c1afe8b6f16dc426be373753c2ff0217248
mathieumunyakazimoise <124375301+mathieumunyakazimoise@users.noreply.github.com>
2023-09-17 19:06:07 +0000

    nnh

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""
-Main file for testing
-"""
+"""Minimum Operations"""
+import math
 
 
 def minOperations(n):
-    """calculates the fewest number of operations needed
-    to result in exactly n H characters in the file"""
+    """Minimum Operations"""
     if n <= 1:
         return 0
-    # copy all means copy = character
-    # paste means character += copy
-    character = 1
-    copy = character
-    min_ops = 0
-    while character < n:
-        # if n is divisible by character, then double it
-        if n % character == 0:
-            # copy all and paste is 2 ops
-            copy = character
-            character = 2 * copy
-            min_ops += 2
-        else:
-            # otherwise add it +1
-            # paste
-            character += copy
-            min_ops += 1
-    return min_ops
-    
+
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
+
+    if n > 1:
+        operations += n
+
+    return operations
